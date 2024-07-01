713de965208a2c1b80e733acebbf85683a515e3c
afhossain <afrina.hossain@gmail.com>
2024-07-01 17:01:51 +0600

    deploymenyt.txt changed

--- myblogproject/deployment.py
+++ myblogproject/deployment.py
@@ -9,6 +9,9 @@ CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
+
+
+
 # WhiteNoise configuration
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -25,15 +28,19 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
-        'PORT': conn_str_params['port'] 
-    }
-}
+# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': conn_str_params['dbname'],
+#         'HOST': conn_str_params['host'],
+#         'USER': conn_str_params['user'],
+#         'PASSWORD': conn_str_params['password'],
+#         'PORT': conn_str_params['port'] 
+#     }
+# }
+
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
