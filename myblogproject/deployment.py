b26549f690ca5989c56d4a99e394d0841b6f2573
afhossain <afrina.hossain@gmail.com>
2024-07-01 15:55:59 +0600

    modification for Render.com

--- myblogproject/deployment.py
+++ myblogproject/deployment.py
@@ -6,7 +6,8 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
 
 # WhiteNoise configuration
 MIDDLEWARE = [
--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -80,12 +80,42 @@ WSGI_APPLICATION = 'myblogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mssql',
+#         'NAME': 'blog',
+#         'USER': 'afhossain2',
+#         'PASSWORD': '123afhossain1234',
+#         'HOST': 'DESKTOP-E1QJHS1',
+#         'OPTIONS': {
+#             'driver': 'ODBC Driver 17 for SQL Server',  # Ensure you have the correct ODBC driver installed
+#             'extra_params': 'TrustServerCertificate=yes;',  # Optional: Can include other parameters like encryption settings
+#         },
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': '123postsa123',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
+
+
+
 
 
 # Password validation
